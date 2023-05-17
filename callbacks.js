fb9b789154722e388a910015685c2f37df9a7b27
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-05-18 00:50:56 +0100

    added js

--- callbacks.js
+++ callbacks.js
@@ -7,7 +7,10 @@ function getPosts(){
     setTimeout(() => {
         let output = '';
         posts.forEach((post, index) => {
-            output += `<li>${post}</li>`;
+            output += `<li>${post.title}</li>`;
         });
+        document.body.innerHTML = output;
     }, 1000);
 }
+
+getPosts();
